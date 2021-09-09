880dbd5dd83ae8002358aff71289b041e570893e
D.K <70153500+DKHexDev@users.noreply.github.com>
2021-09-09 17:30:11 +0200

    Update

--- src/Controller/DemoController.php
+++ src/Controller/DemoController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,12 +20,17 @@ class DemoController extends AbstractController
     }
 
     /**
-     * @Route("/list/{page}", name="demo_list", requirements={"page": "\d+"})
+     * @Route("/discover", name="demo_list")
      */
-    public function list($page)
+    public function list()
     {
         return $this->render('demo/list.html.twig', [
-            'page' => $page,
+            'page' => 'page 1',
+            'cars' => [
+                ['brand' => "Renault"],
+                ['brand' => "Peugeot"],
+                ['brand' => "Citroen"],
+            ]
         ]);
     }
 
@@ -35,6 +41,34 @@ class DemoController extends AbstractController
     {
         return $this->render('demo/show.html.twig', [
             'title' => $title,
+        ]);
+    }
+
+    /**
+     * @Route("/discover/create", name="demo_create")
+     */
+    public function create(Request $request)
+    {
+        // Avec symfony, on n'utilise plus $_GET ou $_POST
+        // On va utiliser un objet request
+        //dump($request);
+        // Equivalent de $_GET['post'] ?? null
+        //dump($request->query->get('page'));
+        //dump($request->get('page')); // Raccourci
+
+        // Equivalent de $_POST['message'] ?? null
+        // dump($request->request->get('message'));
+        // dump($request->get('message'));
+
+        if($request->get('message') === 'secret')
+        {
+            $this->addFlash('success', 'Le message a été envoyé');
+            // return $this->redirect('https://google.fr');
+            return $this->redirectToRoute('demo_list');
+        }
+
+        return $this->render('demo/create.html.twig', [
+            'message' => $request->get('message'),
         ]);
     }
 }
